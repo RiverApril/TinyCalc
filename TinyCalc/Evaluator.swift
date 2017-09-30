474d9e2d09392b834abbc01c5db7c9c7fc114650
Braeden Atlee <nedearb@gmail.com>
2017-09-29 18:04:30 -0700

    First evaluator overhaul
    Needs extensive testing

--- TinyCalc/Evaluator.swift
+++ TinyCalc/Evaluator.swift
@@ -18,27 +18,23 @@ class Expression : Symbol{
     var symbols = [Symbol]()
     var parent: Expression? = nil
     
-    func addNumber(number: String) {
-        symbols.append(SymbolNumber(number: number))
+    func addNumber(_ number: String) {
+        symbols.append(SymbolNumber(number))
     }
     
-    func addExpression(exp: Expression) {
+    func addExpression(_ exp: Expression) {
         exp.parent = self
         symbols.append(exp)
     }
     
-    func addOperator(op: String) {
-        symbols.append(SymbolOperator(op: op))
-    }
-    
-    func addFunction(name: String) {
-        symbols.append(SymbolFunction(name: name))
+    func addOperator(_ op: Character) {
+        symbols.append(SymbolOperator(op))
     }
 }
 
 class SymbolNumber : Symbol{
     
-    init(number: String){
+    init(_ number: String){
         self.number = number
     }
     
@@ -47,14 +43,14 @@ class SymbolNumber : Symbol{
 
 class SymbolOperator : Symbol{
     
-    init(op: String){
+    init(_ op: Character){
         self.op = op
     }
     
-    var op: String
+    var op: Character
 }
 
-class SymbolFunction : Symbol{
+class SymbolFunction : Expression {
     
     init(name: String){
         self.name = name
@@ -74,32 +70,26 @@ class Evaluator {
                     switch op.op{
                     case "+":
                         result = l + r
-                        break
                     case "-":
                         result = l - r
-                        break
                     case "*":
                         result = l * r
-                        break
                     case "/":
                         result = l / r
                         if(r == 0){
                             throw "Division by zero"
                         }
-                        break
                     case "^":
                         result = pow(l, r)
-                        break
                     case "%":
                         result = l.truncatingRemainder(dividingBy:r)
                         if(r == 0){
                             throw "Division by zero"
                         }
-                        break
                     default:
                         throw "Unknown operator"
                     }
-                    let num = SymbolNumber(number: String(result))
+                    let num = SymbolNumber(String(result))
                     expression.symbols.remove(at: i-1) // remove the left
                     i -= 1 // move to stay on operator
                     expression.symbols.remove(at: i) // remove operator, now on right
@@ -119,7 +109,7 @@ class Evaluator {
     static func evaluateExpression(expression: Expression) throws -> SymbolNumber{
         
         if expression.symbols.count == 0{
-            throw ""
+            throw "Empty Expression"
         }
         
         // Evaluate (...)
@@ -150,6 +140,17 @@ class Evaluator {
                     try i = evaluateOperatorHere(op: op, expression: expression, initalI: i)
                 }
             }
+            if i < expression.symbols.count-1 {
+                if let left = expression.symbols[i] as? SymbolNumber, let right = expression.symbols[i+1] as? SymbolNumber {
+                    if let l = Double(left.number), let r = Double(right.number) {
+                        let result = l * r
+                        let num = SymbolNumber(String(result))
+                        expression.symbols.remove(at: i) // remove left number
+                        expression.symbols[i] = num // set current number
+                        i -= 1 // go left one
+                    }
+                }
+            }
             i += 1
         }
         
@@ -165,134 +166,230 @@ class Evaluator {
         }
         
         if expression.symbols.count != 1{
-            throw "evaluation error"
+            throw "Evaluation Failed to Reduce"
         }
         if !(expression.symbols[0] is SymbolNumber){
-            throw "evaluation error"
+            throw "Evaluation Failed to Reduce to Number"
         }
         
-        return expression.symbols[0] as! SymbolNumber
         
+        if let function = expression as? SymbolFunction {
+            return try SymbolNumber(evaluateFunction(name: function.name, number: (expression.symbols[0] as! SymbolNumber).number))
+        } else {
+            return expression.symbols[0] as! SymbolNumber
+        }
+        
+    }
+    
+    static func evaluateFunction(name: String, number: String) throws -> String {
+        if let num = Double(number) {
+            switch name {
+            case "sin":
+                return "\(sin(num))"
+            case "cos":
+                return "\(cos(num))"
+            case "tan":
+                return "\(tan(num))"
+            case "asin":
+                return "\(asin(num))"
+            case "acos":
+                return "\(acos(num))"
+            case "atan":
+                return "\(atan(num))"
+            case "sinh":
+                return "\(sinh(num))"
+            case "cosh":
+                return "\(cosh(num))"
+            case "tanh":
+                return "\(tanh(num))"
+            case "asinh":
+                return "\(asinh(num))"
+            case "acosh":
+                return "\(acosh(num))"
+            case "atanh":
+                return "\(atanh(num))"
+            case "sqrt":
+                return "\(sqrt(num))"
+            case "cbrt":
+                return "\(cbrt(num))"
+            case "abs":
+                return "\(abs(num))"
+            case "floor":
+                return "\(floor(num))"
+            case "ceil":
+                return "\(ceil(num))"
+            case "round":
+                return "\(round(num))"
+            case "log":
+                return "\(log10(num))"
+            case "logb":
+                return "\(log2(num))"
+            case "ln":
+                return "\(log(num))"
+            default:
+                return "Unknown function: \(name)"
+            }
+        } else {
+            throw "Function Contents not Number"
+        }
+    }
+    
+    class ParseStatus {
+        
+        init(str: String) {
+            self.str = str
+        }
+        
+        func char(offset: Int = 0) -> Character{
+            let index = str.index(str.startIndex, offsetBy: i+offset)
+            return str[index]
+        }
+        
+        var str: String
+        var exp = Expression()
+        var i = 0
     }
     
     static func parseExpression(input: String) throws -> Expression {
         
-        var filteredInput = (input as NSString).replacingOccurrences(of: "pi", with:"(\(M_PI))");
-        filteredInput = (filteredInput as NSString).replacingOccurrences(of: "e", with:"(\(M_E))");
-        filteredInput = (filteredInput as NSString).replacingOccurrences(of: " ", with:"");
+        let filteredInput = (input as NSString).replacingOccurrences(of: " ", with:"");
         
-        var exp = Expression()
+        let status = ParseStatus(str: filteredInput)
         
-        var buildingNumber: String = ""
-        var justAddedValue = false
-        var justClosedExpression = false
-        
-        for c in filteredInput.characters {
-            
-            switch c{
-            case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
-                buildingNumber.append(c)
-                continue
-            case ".":
-                if buildingNumber.contains(".") {
-                    throw "Too many '.'"
-                } else {
-                    buildingNumber.append(".")
-                }
-                continue
-            default:
-                if c == "-" {
-                    if buildingNumber.isEmpty && exp.symbols.count >= 2 {
-                        if let op = exp.symbols[exp.symbols.count-1] as? SymbolOperator{
-                            if op.op == "^" || op.op == "/" || op.op == "*"{
-                                buildingNumber.append("-")
-                                continue
-                            }
+        while (status.i < status.str.characters.count) {
+            let char = status.char()
+            switch(char){
+                case "-":
+                    if status.exp.symbols.count > 0 {
+                        if !(status.exp.symbols.last is SymbolOperator) {
+                            try addOperator(status)
+                            break
                         }
                     }
-                    if buildingNumber.isEmpty && justClosedExpression == false && justAddedValue == false {
-                        exp.addNumber(number: "-1")
-                        exp.addOperator(op: "*")
-                        continue
+                    fallthrough
+                case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".":
+                    try addNumber(status)
+                case "+", "*", "/", "^", "%":
+                    try addOperator(status)
+                case "(":
+                    let newExp = Expression()
+                    status.exp.addExpression(newExp)
+                    status.exp = newExp
+                    status.i += 1
+                case ")":
+                    if let parent = status.exp.parent {
+                        status.exp = parent
+                    } else {
+                        // Allow too many closing parentheses for ease of use
+                        let newExp = Expression()
+                        newExp.addExpression(status.exp);
+                        status.exp = newExp;
                     }
-                }
-                if !buildingNumber.isEmpty{
-                    if buildingNumber.characters.count == 1 && (buildingNumber == "." || buildingNumber == "-") {
-                        throw "Number incomplete"
+                    status.i += 1
+                default:
+                    if char >= "a" && char <= "z" {
+                        try addFunctionOrConstant(status)
+                    } else {
+                        throw "Unknown symbol: \"\(char)\""
                     }
-                    if justClosedExpression {
-                        exp.addOperator(op: "*")
-                        justClosedExpression = false
+            }
+        }
+        
+        while(status.exp.parent != nil){
+            status.exp = status.exp.parent!
+        }
+        
+        return status.exp
+    }
+    
+    static func addNumber(_ status: ParseStatus) throws {
+        
+        var number = ""
+        
+        var building = true
+        
+        while (status.i < status.str.characters.count && building) {
+            let char = status.char()
+            switch(char) {
+                case "-":
+                    if number.contains("-") {
+                        throw "Unexpected \"-\""
+                    } else {
+                        number.append(char)
                     }
-                    exp.addNumber(number: buildingNumber)
-                    buildingNumber = ""
-                    justAddedValue = true
-                }
+                case ".":
+                    if number.contains(".") {
+                        throw "Unexpected \".\""
+                    } else {
+                        number.append(char)
+                    }
+                case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
+                    number.append(char)
+                default:
+                    building = false
+            }
+            if building {
+                status.i += 1
+            }
+        }
+        
+        if number == "." {
+            throw "Number Incomplete"
+        }
+        
+        status.exp.addNumber(number)
+    }
+    
+    static func addOperator(_ status: ParseStatus) throws {
+        
+        let char = status.char()
+        switch(char) {
+            case "+", "-", "*", "/", "%", "^":
+                status.exp.addOperator(char)
+            default:
+                throw "Unknown Operator: \"\(char)\""
+        }
+        status.i += 1
+        
+    }
+    
+    static func addFunctionOrConstant(_ status: ParseStatus) throws {
+        
+        var name = ""
+        
+        var isFunc = false
+        
+        while (status.i < status.str.characters.count) {
+            let char = status.char()
+            if char >= "a" && char <= "z" {
+                name.append(char)
+                status.i += 1
+            } else if char == "(" {
+                isFunc = true
+                break
+            } else {
+                isFunc = false
                 break
             }
-            
-            switch c{
-            case "(":
-                if justAddedValue || justClosedExpression{
-                    exp.addOperator(op: "*")
-                    justClosedExpression = false
-                    justAddedValue = false
-                }
-                let newExp = Expression()
-                exp.addExpression(exp: newExp)
-                exp = newExp
-                continue
-            case ")":
-                if exp.symbols.count > 0 && exp.symbols[exp.symbols.count-1] is SymbolOperator{
-                    throw "Unexpected ')'"
-                }
-                if let parent = exp.parent {
-                    exp = parent
-                }else{
-                    //throw "Too many ')'s"
-                    let newExp = Expression()
-                    newExp.addExpression(exp: exp);
-                    exp = newExp;
-                }
-                justAddedValue = true
-                justClosedExpression = true
-                continue
-            case "+", "-", "*", "/", "^", "%":
-                if !justAddedValue{
-                    throw "Unexpected operator: " + String(c)
-                }else{
-                    exp.addOperator(op: String(c))
-                    justAddedValue = false
-                    justClosedExpression = false
-                }
-                
-                continue
-            case ",":
-                justAddedValue = false
-                continue
-            default:
-                throw "Unknown Symbol: " + String(c)
-            }
-            
-        }
-        if !buildingNumber.isEmpty{
-            if buildingNumber.characters.count == 1 && (buildingNumber == "." || buildingNumber == "-") {
-                throw "Number incomplete"
-            }
-            if justClosedExpression {
-                exp.addOperator(op: "*")
-                justClosedExpression = false
-            }
-            exp.addNumber(number: buildingNumber)
-            buildingNumber = ""
-            justAddedValue = true
         }
         
-        while let parent = exp.parent {
-            exp = parent
+        if isFunc {
+            let newExp = SymbolFunction(name: name)
+            status.exp.addExpression(newExp) 
+            status.exp = newExp
+        } else {
+            var num = 0.0
+            switch(name) {
+                case "pi":
+                    num = M_PI
+                case "e":
+                    num = M_E
+                default:
+                    throw "Unknown constant \"\(name)\""
+            }
+            status.exp.addNumber("\(num)")
         }
         
-        return exp
     }
     
     static func evaluate(input: String) throws -> String{
